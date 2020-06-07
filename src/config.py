7c2f9806434a67b9c043c7886c14c90dfaee8d79
Gagan Daroach <daroachgb@msoe.edu>
2020-06-06 22:46:53 -0500

    rosie

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+"""Global configuration."""
+
+#----------------------------------------------------------------------------
+# Paths.
+
+result_dir = '/data/mcw_research/stylegan/results'
+#data_dir = '/srv/hosthome/mcw_8x_he_prostate_stylegan/tfrecord'
+cache_dir = '/data/mcw_research/cache'
+run_dir_ignore = ['results', 'datasets', 'cache']
+data_dir = '/data/mcw_research/tiles/tfrecords'
+#----------------------------------------------------------------------------
